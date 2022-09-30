0ac2032dcf1a984e787ff4fa4956129d009e0b8d
AKX Labz <97577935+quantosnetwork@users.noreply.github.com>
2022-09-30 17:45:44 -0400

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -65,7 +65,7 @@ watch(() => isAuthenticated, () => dialog = false)
       <div>balance: {{ balance ?? 'null' }} matics</div>
       <div>identity: {{ id ?? 'null' }}</div>
       <div>labz balance: {{ labzbalance ?? 'null' }} LABZ</div>
-      <div><v-btn @click="buyLabz(amountMatics)">buy labz</v-btn> <v-text-field name="amount" placeholder="amount of matic" v-model="amountMatics" ></v-text-field></div>
+      <div><v-btn @click="buyLabz(amountMatics)">buy labz</v-btn> <v-text-field label="amount of matic" type="numeric" v-model="amountMatics" ></v-text-field></div>
       <div><v-btn @click="addToken">add token to metamask</v-btn></div>
     </v-container>  
       <v-container>    <v-row>
@@ -79,6 +79,7 @@ watch(() => isAuthenticated, () => dialog = false)
 </template>
 <script lang="ts">
   export default {
+   
     data() {
       return {
         amountMatics: 0
